97eda38ade741368f633bf8fded966910dcd73fc
pradeep1109 <57917040+pradeep1109@users.noreply.github.com>
2021-12-06 12:27:29 -0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/i was changed/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/i was changed/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render (<App/>, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// })
